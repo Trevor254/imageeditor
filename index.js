55c590f6c1a163868be12c8fc2db8956684b04df
trevor <trevoromondi607@gmail.com>
2024-10-21 10:34:47 +0300

    added functionalities

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const fileInput = document.querySelector(".file-input"),
+previewImg = document.querySelector(".preview-img img")
 chooseImage = document.querySelector(".choose-img")
 
 const loadImage = () => {
     let file = fileInput.files[0] //getting the selected file from the user
+    if(!file) return //set to default if user has not selected file
+    previewImg.src = URL.createObjectURL(file) // this method creates a URL of passed file object
+    //console.log(file)
+    //removing disable class once user selects image
+    previewImg.addEventListener("load",()=>{
+        document.querySelector(".container").classList.remove("disable")
+    })
 }
 
-
+//disabling all edit options if user has not selected any image 
 fileInput.addEventListener("change",loadImage)
 
 //aadding event listener to the chooseImage functionality
